df10ddf5838c80497e9531d289d1fff0ae146eac
inon56 <leino333@gmail.com>
2021-03-10 15:56:41 +0200

    c_commit1

---
+++ cmake-build-debug/CMakeFiles/first_prog.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_prog.dir/main.c.o"
+  "first_prog"
+  "first_prog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/first_prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+//    ex1:
+//    pid_t cpid = fork();
+//    if(cpid)
+//        printf("i am the father ");
+//    else
+//        printf("i am the son ");
+
+//    ex2:
+//    if(fork())
+//        if(fork())
+//            if(fork)
+//                printf("hi");
+//
+    fork();
+    fork();
+    fork();
+    return 0;
+}
